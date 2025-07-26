c171e61fed4d19203d8908a8617892f077e4ccae
Daniyal <daniyalkamran33@gmail.com>
2025-07-26 14:39:44 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import 'dotenv/config'
 import dbConnection from './db/index.js'
+import express from 'express'
+
+const app = express()
+
+console.log(process)
 
 dbConnection()
+.then(()=>{
+ app.listen(process.env.PORT || 8000)
+})
